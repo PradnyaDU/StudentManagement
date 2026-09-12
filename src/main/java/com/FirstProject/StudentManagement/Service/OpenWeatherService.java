ae4a14b42044ba858d3abb83ea99c8894455963c
Pradnya <deshpandepradnya18@gmail.com>
2026-09-12 23:08:55 +0530

    new changes for intergrating External API

--- src/main/java/com/FirstProject/StudentManagement/Service/OpenWeatherService.java
+++ src/main/java/com/FirstProject/StudentManagement/Service/OpenWeatherService.java
@@ -2,21 +2,25 @@ package com.FirstProject.StudentManagement.Service;
 
 import com.FirstProject.StudentManagement.apiresponse.WeatherAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OpenWeatherService {
-    private static final String API_KEY = "311fc328d71a263c6091c7656e6e797c"; // Replace with your
-    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+    @Value("${WEATHER_API_KEY}")
+    private String API_KEY;
+    @Value("${WEATHER_API_URL}")
+    private String BASE_URL;
 
     @Autowired
     private RestTemplate restTemplate;
+
     public WeatherAPIResponse getWeatherData(double lat, double lon) {
-        String url = BASE_URL.replace("{lat}", String.valueOf(lat)).replace("{lon}", String.valueOf(lon)).replace("{API key}", API_KEY);
-        return restTemplate.exchange(url, HttpMethod.GET, null, WeatherAPIResponse.class).getBody();
+
+        String url = BASE_URL + "?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+        return restTemplate.exchange(url, HttpMethod.GET, null,WeatherAPIResponse.class, lat, lon, API_KEY).getBody();
     }
-
-
 }
